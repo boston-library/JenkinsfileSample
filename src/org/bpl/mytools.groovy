7af47f5432507ce80d4f71afe480e50b6ef7f8c9
miao-bpl <rmiao@bpl.org>
2023-12-08 14:43:05 -0500

    Add Jenkinsfile

--- src/org/bpl/mytools.groovy
+++ src/org/bpl/mytools.groovy
@@ -3,10 +3,11 @@ package org.bpl
 def GetCode(srcType,branchName,gitHttpURL,credentialsId){
 	if (srcType == "git"){
 		println("Downloading code from branch: ${branchName}")
-		// checkout([$class: GitSCM, branches: [[name: "${branchName}"]], 
-		// 	extensions: [],
-		// 	// userRemoteConfigs: [[credentialsId: "{$credentialsId}", url: "${gitHttpURL}"]]
-		// 	userRemoteConfigs: [[url: "${gitHttpURL}"]]
-		// 	])
+		checkout([
+			$class: 'GitSCM', branches: [[name: "${branchName}"]], 
+			extensions: [],
+			// userRemoteConfigs: [[credentialsId: "{$credentialsId}", url: "${gitHttpURL}"]]
+			userRemoteConfigs: [[url: "${gitHttpURL}"]]
+			])
 	}
 }
